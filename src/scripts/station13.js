91638127216956ede937ae6a058a2f128ffb56ec
tech-kinako <tech.kinako@gmail.com>
2024-07-15 10:47:58 +0900

    clear station13

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,10 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  const isCheck = document.getElementById("check");
+  const target = document.getElementById("text");
+  if (isCheck.checked) {
+      target.style.backgroundColor = "red";
+  } else {
+    target.style.backgroundColor = "transparent";
+  }
 }
